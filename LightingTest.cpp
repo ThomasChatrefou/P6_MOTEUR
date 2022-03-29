d889cdd650fcabc614ff12d202776b6a7141a086
ScoopIndustries <91889058+ScoopIndustries@users.noreply.github.com>
2022-03-29 15:09:42 +0200

    Commenting

--- LightingTest.cpp
+++ LightingTest.cpp
@@ -1,13 +1,12 @@
 #include "LightingTest.hpp"
 
-
+#include "imgui.h"
 #include "Time.hpp"
 #include "Renderer.hpp"
 #include "GUI.hpp"
 
 #ifndef ShaderFile
 #define ShaderFile
-//const std::string SHADER_FILE = "resources/shaders/Basic.shader";
 const std::string SHADER_FILE = "resources/shaders/BasicLighting.shader";
 #endif // !ShaderFile
 
@@ -21,31 +20,40 @@ const std::string CTHULHU_TEXTURE_FILE = "resources/textures/Cthulhu_Texture.png
 const std::string CTHULHU_MESH_FILE = "resources/models/Cthulhu.fbx";
 #endif // !CthulhuMeshFile
 
-LightingTest::LightingTest(const AppSystemData& appData) : m_Translation(0.0f, 0.0f, 0.0f), m_Rotation(0.0f, 0.0f, 0.0f)
+LightingTest::LightingTest(const AppSystemData& appData) 
+    : m_Translation(0.0f, 0.0f, 0.0f), m_Rotation(0.0f, 0.0f, 0.0f), lightColor{ 1.0f, 1.0f, 1.0f, 1.0f }, m_SphericalCoord(10.0f, 45.0f, 45.0f)
 {
+    //-------------------Directory------------------------------//
+
     app = appData;
     auto shaderPath = app.srcPath + SHADER_FILE;
     auto texturePath = app.srcPath + CTHULHU_TEXTURE_FILE;
     auto meshPath = app.srcPath + CTHULHU_MESH_FILE;
-	lightPos = glm::vec3(0.0f, 0.5f, 0.0f);
+
+    //-------------------Directory------------------------------//
 
     //temp -> put this into scene
     glm::vec3 camPos = glm::vec3(0.0f, 0.0f, -10.0f);
     glm::vec3 target = glm::vec3(0.0f, 0.0f, 0.0f);
     float fov = 45.0f;
 
+    //-------------------Init Light------------------------------//
+
+    lightPos = glm::vec3(0.0f, 0.5f, 0.0f);
     ambientStrength = 0.1f;
     specularStrength = 1.0f;
     specularPower = 6;
+
+    //-------------------View------------------------------//
 
     m_Camera = std::make_unique<Camera>();
     m_Camera->OnInit(camPos, target, fov, (float)app.winWidth / (float)app.winHeight, 0.1f, 100.0f);
     m_Proj = m_Camera->GetProjectionMatrix();
     m_View = m_Camera->GetLookAtMatrix();
+    //----------------------------------------------------------------------------------------------//
 
     m_Mesh = std::make_unique<Mesh>(meshPath);
     m_Material = std::make_unique<Material>(shaderPath, texturePath);
-
     m_Mesh->Unbind();
     m_Material->Unbind();
 }
@@ -61,16 +69,31 @@ void LightingTest::OnLoop(float deltaTime)
 
 void LightingTest::OnRender()
 {
-    m_Material->Bind();
+    //-------------------------------Camera Spherique----------------------------------------//
 
+    r = m_SphericalCoord.x;
+    theta = m_SphericalCoord.y;
+    phi = m_SphericalCoord.z;
+    glm::vec3 camPosition = glm::vec3(
+        r * glm::cos(glm::radians(theta)) * glm::sin(glm::radians(phi)),
+        r * glm::cos(glm::radians(phi)),
+        r * glm::sin(glm::radians(theta)) * glm::sin(glm::radians(phi)));
+    m_Camera->SetPosition(camPosition);
+    glm::mat4 proj = m_Camera->GetProjectionMatrix();
+    glm::mat4 view = m_Camera->GetLookAtMatrix();
+
+    //-------------------------------Shader----------------------------------------//
+
+    m_Material->Bind();
     m_Material->SetVec3("objectColor", 1.0f, 1.0f, 1.0f);
-    m_Material->SetVec3("lightColor", 1.0f, 1.0f, 1.0f);
+    m_Material->SetVec3("lightColor", lightColor[0], lightColor[1], lightColor[2]);
     m_Material->SetVec3("lightPos", lightPos.x, lightPos.y, lightPos.z);
     m_Material->SetVec3("viewPos", m_Camera->GetPosition().x, m_Camera->GetPosition().y, m_Camera->GetPosition().z);
-
     m_Material->SetFloat("ambientStrength", ambientStrength);
     m_Material->SetFloat("specularStrength", specularStrength);
     m_Material->SetInt("specularPower", std::pow(2, specularPower));
+
+    //-------------------------------Matrice----------------------------------------//
 
     glm::mat4 model = glm::mat4(1.0f);
     model = glm::rotate(model, glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
@@ -80,25 +103,43 @@ void LightingTest::OnRender()
     model = glm::rotate(model, m_Rotation.y, glm::vec3(0.0f, 1.0f, 0.0f));
     model = glm::rotate(model, m_Rotation.z, glm::vec3(0.0f, 0.0f, 1.0f));
     model = glm::translate(model, m_Translation);
-    glm::mat4 mvp = m_Proj * m_View * model;
+    glm::mat4 mvp = proj * view * model;
 
+    //-----------------------------------------------------------------------------//
     m_Material->UpdateMVP(mvp);
     app.pRenderer->Draw(*m_Mesh, *m_Material);
 }
 
 void LightingTest::OnGuiRender()
 {
+    //----------------------------IMGUI WINDOW-------------------------------//
     app.pGUI->SetFixedWindowSize(250.0f, 100.0f);
     app.pGUI->PrintFPS(app.pClock->getDeltaTime());
-    app.pGUI->BeginWindow("Movements", 520.0f, 0.0f, 500.0f, 100.0f);
 
+    //----------------------------Model Movement-------------------------------//
+
+    app.pGUI->BeginWindow("Movements", 520.0f, 0.0f, 500.0f, 250.0f);
+    //------------------------------------------------------------------------------------//
     app.pGUI->AddSliderFloat3("Translation", m_Translation, -10.0f, 10.0f);
     app.pGUI->AddSliderFloat3("Rotation", m_Rotation, 0.0f, glm::radians(360.0f));
+    app.pGUI->AddSliderFloat3("Light Position", lightPos, -10.0f, 10.0f);
+   //--------------Orbital Camera-------------//
+    ImGui::Text("Spherical movements");
+    app.pGUI->AddSliderFloat("Distance", m_SphericalCoord.x, 0.0f, 10.0f);
+    app.pGUI->AddSliderFloat("Longitude", m_SphericalCoord.y, 0.0f, 360.0f);
+    app.pGUI->AddSliderFloat("Colatitude", m_SphericalCoord.z, 0.0f, 180.0f);
+    //------------------------------------------------------------------------------------//
     app.pGUI->EndWindow();
 
-    app.pGUI->BeginWindow("Light Settings", app.winWidth - 500.0f, 0.0f, 500.0f, 150.0f);
+
+    //----------------------------Light Settings-------------------------------//
+
+    app.pGUI->BeginWindow("Light Settings", app.winWidth - 500.0f, 0.0f, 500.0f, 200.0f);
+    //------------------------------------------------------------------------------------//
     app.pGUI->AddSliderFloat("Ambiant Strength", ambientStrength, 0.0f, 1.0f);
     app.pGUI->AddSliderFloat("Specular Strength", specularStrength, 0.0f, 1.0f);
     app.pGUI->AddSliderInt("Specular Power", specularPower, 0, 8);
+    app.pGUI->AddColorEdit4("Light Color", lightColor);
+    //-----------------------------------------------------------------------------------//
     app.pGUI->EndWindow();
 }
--- LightingTest.hpp
+++ LightingTest.hpp
@@ -17,17 +17,22 @@ public:
 	void OnGuiRender() override;
 
 private:
-	glm::vec3 lightPos;
-
 	glm::vec3 m_Translation;
 	glm::vec3 m_Rotation;
 	glm::mat4 m_Proj;
 	glm::mat4 m_View;
 
+	//Camera Orbital	
+	float r, theta, phi;
+	glm::vec3 m_SphericalCoord;
+
+
+	//Lighting
+	glm::vec3 lightPos;
+	float lightColor[4];
 	float ambientStrength;
 	float specularStrength;
 	int specularPower;
-
 
 	std::unique_ptr<Camera> m_Camera;
 	std::unique_ptr<Mesh> m_Mesh;
